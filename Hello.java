b9118841e7532aae5b814478c0ed8cc61665c86e
vignesh <vickypmv@gmail.com>
2018-05-09 03:23:50 -0400

    new line added

--- Hello.java
+++ Hello.java
@@ -2,7 +2,9 @@ public class Hello
 {
 public static void main(String args[])
 {
+	String str="Sivaxa";
 	for(int i=1;i<=10;i++)
 	System.out.println("Welcome Continous Integration:"+i);
+	System.out.println(str);
 }
 }
