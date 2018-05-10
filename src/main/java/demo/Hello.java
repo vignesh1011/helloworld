9b56a7834ce8d6d4620032416a68fe34edc7c59b
vignesh <vickypmv@gmail.com>
2018-05-10 08:58:25 -0400

    gradle script

---
+++ src/main/java/demo/Hello.java
@@ -0,0 +1,12 @@
+package demo;
+import org.joda.time.LocalTime;
+public class Hello
+{
+public static void main(String args[])
+{
+	LocalTime time = new LocalTime();
+	String str="Sivaxa";
+	System.out.println(str);
+	System.out.println("Local time is :"+ time );
+}
+}
